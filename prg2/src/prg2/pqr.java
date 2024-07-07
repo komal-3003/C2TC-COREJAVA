90de9caf41e0e2d9c44f7fb72c4f842daec5cb43
pavan <pavan@PavanMaladkar>
2024-07-07 14:32:00 +0530

    First commit

---
+++ prg2/src/prg2/pqr.java
@@ -0,0 +1,12 @@
+package prg2;
+class pqr  
+{    
+	public static void main(String[] args)  
+	{    int number = 5;    int factorial = number;   
+	for(int i =(number - 1); i > 1; i--)  
+	{     
+		factorial = factorial * i;  
+	}   
+System.out.println("Factorial of a number is:" + factorial);   
+}
+}  
